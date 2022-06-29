ca68af160de3c180c8b366c555571785a75cc93b
Daniele Ricci <daniele.icc@gmail.com>
2022-06-29 23:02:30 +0000

    minor fixes

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,5 @@ module.exports = {
   preset:                 "ts-jest",
   testEnvironment:        "jest-environment-node-single-context",
   testPathIgnorePatterns: ["/node_modules/"],
-  testSequencer:          "../testSequencer.js"
+  testSequencer:          "./testSequencer.js"
 };
